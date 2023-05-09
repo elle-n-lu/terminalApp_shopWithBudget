d6ce05a2f91084d353b074e3fcd08fe044fbf2ce
elle-n-lu <joew1960938924@gmail.com>
2023-05-09 22:34:55 +0930

    add more comments for userinput testing

--- src/test/userinpu_test.py
+++ src/test/userinpu_test.py
@@ -7,12 +7,14 @@ shoplist = {'Egg': ['700g', 'large'],
             'Apple': ['1kg'],
             'milk': ['2L']}
 
+# initalize the userinput object, set the scope to be module, so the function following can be called as the attribute of the object 
 @pytest.fixture(scope='module')
 def createUserInpu():
     return userinput()
 
 
 def test_get_budget( monkeypatch):
+    # mock user input as 100
     monkeypatch.setattr('builtins.input', lambda _: "100")
     budget = float(input('print your budget:'))
     assert budget == float('100')
